743c3f6d707bf7fcf55faaf31de40c2faf1b7394
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-01 09:26:34 +0100

    Update updoot.sh

--- updoot.sh
+++ updoot.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
     #Sync pacman database
-                sudo pacman -Syy
+        sudo pacman -Syy
 
     #Clean pacman cache
-                sudo pacman -Scc --noconfirm
+        sudo pacman -Scc --noconfirm
 
     #Remove unused packages
-                sudo pacman -Rns $(pacman -Qtdq) --noconfirm
+        sudo pacman -Rns $(pacman -Qtdq) --noconfirm
 
     #Delete cache directory
-                sudo rm -rf ~/.cache*
+        sudo rm -rf ~/.cache*
 
     #Remove dotfiles/ .bashrc .zshrc .xinitrc .config/ from $HOME
-                sudo rm -rf $HOME/dotfiles
-                sudo rm -rf $HOME/.bashrc
-                sudo rm -rf $HOME/.zshrc
-                sudo rm -rf $HOME/.xinitrc
-                sudo rm -rf $HOME/.config
+        sudo rm -rf $HOME/dotfiles
+        sudo rm -rf $HOME/.bashrc
+        sudo rm -rf $HOME/.zshrc
+        sudo rm -rf $HOME/.xinitrc
+        sudo rm -rf $HOME/.config
 
     #Clone dotfiles.git & copy .bashrc .xinitrc .config/ to $HOME
-                sudo git clone https://github.com/maisl0l//dotfiles
-                sudo cp -r $HOME/dotfiles/.bashrc $HOME/.bashrc
-                sudo cp -r $HOME/dotfiles/.xinitrc $HOME/.xinitrc
-                sudo cp -r $HOME/dotfiles/.config $HOME/.config
+        sudo git clone https://github.com/maisl0l//dotfiles
+        sudo cp -r $HOME/dotfiles/.bashrc $HOME/.bashrc
+        sudo cp -r $HOME/dotfiles/.xinitrc $HOME/.xinitrc
+        sudo cp -r $HOME/dotfiles/.config $HOME/.config
 
     #Neofetch test
-                sudo cp -r $HOME/dotfiles/test/config.conf $HOME/.config/neofetch/config.conf
-                sudo cp -r $HOME/dotfiles/test/ascii $HOME/.config/neofetch/logo
+        sudo cp -r $HOME/dotfiles/test/config.conf $HOME/.config/neofetch/config.conf
+        sudo cp -r $HOME/dotfiles/test/ascii $HOME/.config/neofetch/logo
 
     #Clear terminal neofetch list-filesystem & done
-                clear
-                neofetch | lolcat
-                df -h | lolcat
-                echo ''
-                echo 'Done :)' | lolcat
-                echo ''
+        clear
+        neofetch | lolcat
+        df -h | lolcat
+        echo ''
