dc1db5bf0ecc5f4b8cb478d9e3f2fbd3dd697a0b
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-01 01:33:48 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+#Sync pacman database
 sudo pacman -Syy
+
+#Clean pacman cache
 sudo pacman -Scc --noconfirm
+
+#Remove unused packages
 sudo pacman -Rns $(pacman -Qtdq) --noconfirm
+
+#Delete cache directory
 sudo rm -rf ~/.cache*
+
+#Clear terminal & list filesystem
 clear
 df -h
