2ac0bf170e72ac940f66b843137f2f267543c561
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-01 07:16:07 +0100

    Update updoot.sh

--- updoot.sh
+++ updoot.sh
@@ -16,8 +16,7 @@ echo 'Do pacman cleanup ?' | lolcat
 
 	#Clear terminal & list filesystem
 	clear 
-	df -h | lolcat;;
-  * )
+	df -h | lolcat
 echo ''
 
 echo 'Delete dotfiles .config .bashrc .zshrc .xinitrc ?' | lolcat
@@ -25,14 +24,14 @@ echo 'Delete dotfiles .config .bashrc .zshrc .xinitrc ?' | lolcat
 	sudo rm -rf $HOME/.bashrc
 	sudo rm -rf $HOME/.zshrc
 	sudo rm -rf $HOME/.xinitrc
-	sudo rm -rf $HOME/.config;;
+	sudo rm -rf $HOME/.config
 echo ''
 
 echo 'Replace .config .bashrc .xinitrc ?' | lolcat
   	sudo git clone https://github.com/maisl0l//dotfiles | lolcat
 	sudo cp -r $HOME/dotfiles/.bashrc $HOME/.bashrc
 	sudo cp -r $HOME/dotfiles/.xinitrc $HOME/.xinitrc
-	sudo cp -r $HOME/dotfiles/.config $HOME/.config;;
+	sudo cp -r $HOME/dotfiles/.config $HOME/.config
 echo ''
 
 echo 'Neofetch test' | lolcat
