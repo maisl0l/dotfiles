3834fc92c366ce48d5bb76f448ce765e360ecc2a
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-01 08:13:01 +0100

    Update updoot.sh

--- updoot.sh
+++ updoot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
   	#Sync pacman database
 	sudo pacman -Syy
 
@@ -12,30 +11,34 @@
 	#Delete cache directory
 	sudo rm -rf ~/.cache*
 echo ''
-	#Remove dotfiles .bashrc .zshrc .xinitrc .config/
+
+	#Remove dotfiles .bashrc .zshrc .xinitrc .config/ from $HOME
   	sudo rm -rf $HOME/dotfiles
 	sudo rm -rf $HOME/.bashrc
 	sudo rm -rf $HOME/.zshrc
 	sudo rm -rf $HOME/.xinitrc
 	sudo rm -rf $HOME/.config
 echo ''
-	#Clone dotfiles.git reaplace .bashrc .xinitrc .config/
+
+	#Clone dotfiles.git reaplace .bashrc .xinitrc .config/ to $HOME
   	sudo git clone https://github.com/maisl0l//dotfiles | lolcat
 	sudo cp -r $HOME/dotfiles/.bashrc $HOME/.bashrc
 	sudo cp -r $HOME/dotfiles/.xinitrc $HOME/.xinitrc
 	sudo cp -r $HOME/dotfiles/.config $HOME/.config
 	sudo chmod +x $HOME/.config/polybar/launch.sh
 echo ''
-
-echo 'Neofetch test' | lolcat
-sudo cp -r $HOME/dotfiles/test/config.conf $HOME/.config/neofetch/config.conf
-sudo cp -r $HOME/dotfiles/test/ascii $HOME/.config/neofetch/logo
+	
+	#Neofetch test
+	sudo cp -r $HOME/dotfiles/test/config.conf $HOME/.config/neofetch/config.conf
+	sudo cp -r $HOME/dotfiles/test/ascii $HOME/.config/neofetch/logo
 echo ''
 
-clear
-neofetch | lolcat
-df -h | lolcat
-
+	#Clear terminal, neofetch and list filesystems
+	clear
+	neofetch | lolcat
+	df -h | lolcat
 echo ''
-echo 'Done :)' | lolcat
+	
+	#Final ok
+	echo 'Done :)' | lolcat
 echo ''
