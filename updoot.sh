7a48e61a1e0edc960a108176984540a3cbe0f0a9
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-01 08:26:33 +0100

    Update updoot.sh

--- updoot.sh
+++ updoot.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
-  	#Sync pacman database
-	sudo pacman -Syy
+    #Sync pacman database
+    sudo pacman -Syy
 
-	#Clean pacman cache
-	sudo pacman -Scc --noconfirm
+    #Clean pacman cache
+    sudo pacman -Scc --noconfirm
 
-	#Remove unused packages
-	sudo pacman -Rns $(pacman -Qtdq) --noconfirm
+    #Remove unused packages
+    sudo pacman -Rns $(pacman -Qtdq) --noconfirm
 
-	#Delete cache directory
-	sudo rm -rf ~/.cache*
+    #Delete cache directory
+    sudo rm -rf ~/.cache*
 echo ''
 
-	#Remove dotfiles/ .bashrc .zshrc .xinitrc .config/ from $HOME
-  	sudo rm -rf $HOME/dotfiles
-	sudo rm -rf $HOME/.bashrc
-	sudo rm -rf $HOME/.zshrc
-	sudo rm -rf $HOME/.xinitrc
-	sudo rm -rf $HOME/.config
+    #Remove dotfiles/ .bashrc .zshrc .xinitrc .config/ from $HOME
+    sudo rm -rf $HOME/dotfiles
+    sudo rm -rf $HOME/.bashrc
+    sudo rm -rf $HOME/.zshrc
+    sudo rm -rf $HOME/.xinitrc
+    sudo rm -rf $HOME/.config
 echo ''
 
-	#Clone dotfiles.git & copy .bashrc .xinitrc .config/ to $HOME
-  	sudo git clone https://github.com/maisl0l//dotfiles | lolcat
-	sudo cp -r $HOME/dotfiles/.bashrc $HOME/.bashrc
-	sudo cp -r $HOME/dotfiles/.xinitrc $HOME/.xinitrc
-	sudo cp -r $HOME/dotfiles/.config $HOME/.config
-	sudo chmod +x $HOME/.config/polybar/launch.sh
+    #Clone dotfiles.git & copy .bashrc .xinitrc .config/ to $HOME
+    sudo git clone https://github.com/maisl0l//dotfiles | lolcat
+    sudo cp -r $HOME/dotfiles/.bashrc $HOME/.bashrc
+    sudo cp -r $HOME/dotfiles/.xinitrc $HOME/.xinitrc
+    sudo cp -r $HOME/dotfiles/.config $HOME/.config
+    sudo chmod +x $HOME/.config/polybar/launch.sh
 echo ''
-	
-	#Neofetch test
-	sudo cp -r $HOME/dotfiles/test/config.conf $HOME/.config/neofetch/config.conf
-	sudo cp -r $HOME/dotfiles/test/ascii $HOME/.config/neofetch/logo
+    
+    #Neofetch test
+    sudo cp -r $HOME/dotfiles/test/config.conf $HOME/.config/neofetch/config.conf
+    sudo cp -r $HOME/dotfiles/test/ascii $HOME/.config/neofetch/logo
 echo ''
 
-	#Clear terminal, neofetch, list filesystem, final ok
-	clear
-	neofetch | lolcat
-	df -h | lolcat
-	echo ''
-	echo 'Done :)' | lolcat
+    #Clear terminal, neofetch, list filesystem, final ok
+    clear
+    neofetch | lolcat
+    df -h | lolcat
+    echo ''
+    echo 'Done :)' | lolcat
 echo ''
