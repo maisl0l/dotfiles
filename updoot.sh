b16fe738b7f4c6e3e98445b54407ae97b21fabeb
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-01 07:16:46 +0100

    Update updoot.sh

--- updoot.sh
+++ updoot.sh
@@ -39,7 +39,7 @@ sudo cp -r $HOME/dotfiles/test/config.conf $HOME/.config/neofetch/config.conf
 sudo cp -r $HOME/dotfiles/test/ascii $HOME/.config/neofetch/logo
 echo ''
 
-df -h | lolcat
+df -h
 
 #echo 'Placeholder' | lolcat
 #read -p "[y/n] ?" choice
