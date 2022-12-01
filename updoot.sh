55371780e0a0bcd40f4b931ab2c5a0188a3f39bd
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-01 03:56:40 +0100

    Update updoot.sh

--- updoot.sh
+++ updoot.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-echo 'Delteting dotfiles .config .bashrc .zshrc .xinitrc' | lolcat
+echo 'Delete dotfiles .config .bashrc .zshrc .xinitrc ?' | lolcat
+
 sudo rm -rf $HOME/dotfiles
-sudo rm -rf $HOME/.config
 sudo rm -rf $HOME/.bashrc
 sudo rm -rf $HOME/.zshrc
 sudo rm -rf $HOME/.xinitrc
+sudo rm -rf $HOME/.config
 echo
 
 echo 'Replacing .config .bashrc .xinitrc' | lolcat
 sudo git clone https://github.com/maisl0l//dotfiles | lolcat
-sudo cp -r $HOME/dotfiles/.config $HOME/.config
 sudo cp -r $HOME/dotfiles/.bashrc $HOME/.bashrc
 sudo cp -r $HOME/dotfiles/.xinitrc $HOME/.xinitrc
+sudo cp -r $HOME/dotfiles/.config $HOME/.config
 echo
 
 echo 'Neofetch test' | lolcat
