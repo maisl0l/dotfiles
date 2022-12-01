1593e8a82e45459eefc8d4712c6f5d9e69959ce0
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-01 03:36:57 +0100

    Update updoot.sh

--- updoot.sh
+++ updoot.sh
@@ -9,7 +9,7 @@ sudo rm -rf $HOME/.xinitrc
 sudo git clone https://github.com/maisl0l//dotfiles
 sudo cp -r $HOME/dotfiles/.config $HOME/.config
 sudo cp -r $HOME/dotfiles/.bashrc $HOME/.bashrc
-sudo cp -r $HOME/dotfiles.xinitrc $HOME/.xinitrc
+sudo cp -r $HOME/dotfiles/.xinitrc $HOME/.xinitrc
 
 sudo cp -r $HOME/dotfiles/test/config.conf $HOME/.config/neofetch/config.conf
 sudo cp -r $HOME/dotfiles/test/ascii $HOME/.config/neofetch/logo
