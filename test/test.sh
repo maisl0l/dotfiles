920f78854b7768d78d059e5e4a78993da454b6fd
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-02 01:02:07 +0100

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@ sudo pacman -Syu --noconfirm
 mkdir /tmp/yay
 cd /tmp/yay
 curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
-sudo makepkg -si –-noconfirm
+makepkg -si --noconfirm
 cd
 rm -rf /tmp/yay
 yay --version
