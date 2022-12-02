89bc77d5e62fee64cbc0f6a639b08e8dc0adce96
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-02 06:59:10 +0100

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+    #archinstall
+    archinstall --config /home/dotfiles/archinstall/user_configuration.json --disk_layouts /home/dotfiles/archinstall/user_disk_layout.json --creds /home/dotfiles/archinstall/user_credentials.json
+
     #Backup package list
         sudo pacman -Qqen > pacman-backup.txt
 
