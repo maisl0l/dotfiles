48ee0cb2498db209c02422404db9864191ced0d6
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-02 07:12:40 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,2 @@
+#archinstall
+    archinstall --config /home/dotfiles/archinstall/user_configuration.json --disk_layouts /home/dotfiles/archinstall/user_disk_layout.json --creds /home/dotfiles/archinstall/user_credentials.json
