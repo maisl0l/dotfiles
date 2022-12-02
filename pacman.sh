2ac8cc743c22adc337f7150a56f1db8247a8401a
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-02 08:06:59 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-sudo pacman -Sy --noconfirm firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi pipewire pipewire-alsa pipewire-jack pipewire-pulse zsh spotifyd 
-clear
-neofetch | lolcat
-echo ''
-echo ' done :)'
-echo ''
+sudo pacman -Sy --noconfirm firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi pipewire pipewire-alsa pipewire-jack pipewire-pulse zsh spotifyd
