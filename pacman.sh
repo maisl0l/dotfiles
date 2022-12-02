065624fdc96dbfaf9711087f6a5864b7cb0cafee
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-02 04:08:55 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-sudo pacman -S - < /misc/pacman.txt
+sudo pacman -S - < 
+firefox kitty picom polybar
+i3-gaps
+neofetch
+vim
+lolcat
+nitrogen
+openssh
+openvpn
+rofi
+pipewire
+pipewire-alsa
+pipewire-jack
+pipewire-pulse
+
