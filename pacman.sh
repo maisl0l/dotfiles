5982a22c4af42df76e2888d46156346bc57a0ff5
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-03 09:26:17 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     #Install pacman packages
-    sudo pacman -Sy --noconfirm qjackctl xdg-user-dirs xclip libqalculate iftop htop aspell scrot dunst android-file-transfer brightnessctl ardour slock pcmanfm network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi pipewire pipewire-alsa pipewire-jack pipewire-pulse zsh spotifyd libreoffice
+    sudo pacman -Sy --noconfirm qjackctl xdg-user-dirs xclip libqalculate iftop htop aspell scrot dunst android-file-transfer brightnessctl ardour slock pcmanfm network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp pipewire pipewire-alsa pipewire-jack pipewire-pulse zsh spotifyd libreoffice
     #cool screenshot thing: shotgun -g $(hacksaw)        
     
     #Install yay
