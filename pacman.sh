f610f69bf2f33c164703c27ec2347d373c29980b
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-03 08:54:10 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
     #Install pacman packages
-    sudo pacman -Sy --noconfirm firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi pipewire pipewire-alsa pipewire-jack pipewire-pulse zsh spotifyd
-        
+    sudo pacman -Sy --noconfirm pcmanfm network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi pipewire pipewire-alsa pipewire-jack pipewire-pulse zsh spotifyd
+    #cool screenshot thing: shotgun -g $(hacksaw)        
+    
     #Install yay
     mkdir /tmp/yay
     cd /tmp/yay
