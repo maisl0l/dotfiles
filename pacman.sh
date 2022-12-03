f36ad5163ff74f0f22c29d020e5d3dd695be1e18
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-03 06:03:34 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 sudo pacman -Sy --noconfirm firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi pipewire pipewire-alsa pipewire-jack pipewire-pulse zsh spotifyd
+sudo pacman -Syu --noconfirm
+mkdir /tmp/yay
+cd /tmp/yay
+curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
+makepkg -si --noconfirm
+cd
+rm -rf /tmp/yay
+yay --version
