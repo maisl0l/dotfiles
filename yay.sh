6f7d5f55356dba0413617c11a6dab80a0b7368bf
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-03 06:03:07 +0100

    Delete yay.sh

--- yay.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo pacman -Syu --noconfirm
-mkdir /tmp/yay
-cd /tmp/yay
-curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
-makepkg -si --noconfirm
-cd
-rm -rf /tmp/yay
-yay --version
