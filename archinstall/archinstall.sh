ac370295da194c5b375fb61737841ea2189d40a1
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-03 07:05:58 +0100

    Update archinstall.sh

--- archinstall/archinstall.sh
+++ archinstall/archinstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
   #Use custom install config
+    sudo pacman -Sy archlinux-keyring
     archinstall --config https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_configuration.json --disk_layouts https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_disk_layout.json --creds https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_credentials.json
