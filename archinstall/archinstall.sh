50c337bfe876a27fe12bd0e71e14c8167bc20fe4
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-03 06:31:54 +0100

    Update archinstall.sh

--- archinstall/archinstall.sh
+++ archinstall/archinstall.sh
@@ -1,1 +1,3 @@
-archinstall --config https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_configuration.json --disk_layouts https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_disk_layout.json --creds https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_credentials.json
+#!/bin/bash
+  #Use custom install config
+    archinstall --config https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_configuration.json --disk_layouts https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_disk_layout.json --creds https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_credentials.json
