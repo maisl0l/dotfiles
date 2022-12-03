8e8b5553bb80a3c7b4e2b3a112e9a19ef7937e1a
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-03 06:13:06 +0100

    Update nuke.sh

--- nuke.sh
+++ nuke.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sudo rm -rf $HOME/dotfiles
-sudo rm -rf $HOME/.config
-sudo rm -rf $HOME/.bashrc
-sudo rm -rf $HOME/.zshrc
-sudo rm -rf $HOME/.xinitrc
-sudo rm -rf /home/Pictures
+
+  #Remove dotfiles
+    sudo rm -rf $HOME/dotfiles
+    sudo rm -rf $HOME/.config
+    sudo rm -rf $HOME/.bashrc
+    sudo rm -rf $HOME/.zshrc
+    sudo rm -rf $HOME/.xinitrc
+    sudo rm -rf /home/Pictures
