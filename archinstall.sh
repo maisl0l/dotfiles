282cee33f4475484db05c427f8bc983d96ff6c00
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-03 07:45:35 +0100

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
     #Use custom install config
+    read -p "Everything is set besides the drives to install to and their partitioning..."
     archinstall --config https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_configuration.json --disk_layouts https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_disk_layout.json --creds https://raw.githubusercontent.com/maisl0l/dotfiles/main/archinstall/user_credentials.json
