a092954128a0ed19577f05a1974ba7691e84a773
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-06 04:13:16 +0100

    Update updoot.sh

--- updoot.sh
+++ updoot.sh
@@ -15,11 +15,11 @@
     sudo rm -rf ~/.cache*
 
     #Remove dotfiles/ .bashrc .zshrc .xinitrc .config/ from $HOME
-    rm -rf $HOME/dotfiles
-    rm -rf $HOME/.bashrc
-    rm -rf $HOME/.zshrc
-    rm -rf $HOME/.xinitrc
-    rm -rf $HOME/.config
+    sudo rm -rf $HOME/dotfiles
+    sudo rm -rf $HOME/.bashrc
+    sudo rm -rf $HOME/.zshrc
+    sudo rm -rf $HOME/.xinitrc
+    sudo rm -rf $HOME/.config
 
     #Clone dotfiles.git & copy .bashrc .zshrc .xinitrc .config/ to $HOME
     git clone https://github.com/maisl0l//dotfiles
