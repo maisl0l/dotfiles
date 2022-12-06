528c6689be99e3c3c7819984c316b02cea100a2d
mais <95695358+maisl0l@users.noreply.github.com>
2022-12-06 21:58:09 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
     #Install pacman packages
-    sudo pacman -Sy --noconfirm cargo ranger ffmpeg mpv discord qjackctl xclip libqalculate iftop htop aspell scrot dunst android-file-transfer brightnessctl ardour slock pcmanfm network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp pipewire pipewire-alsa pipewire-jack pipewire-pulse zsh spotifyd libreoffice
+    sudo pacman -Sy --noconfirm cargo ranger ffmpeg mpv discord qjackctl xclip libqalculate iftop htop aspell scrot dunst android-file-transfer brightnessctl ardour slock pcmanfm network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp zsh spotifyd libreoffice
     
-    # xdg-user-dirs
+    # xdg-user-dirs pipewire pipewire-alsa pipewire-jack pipewire-pulse 
     
     #Install yay
     mkdir /tmp/yay
