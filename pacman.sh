50e0ffcd46239a74ba1059dbd3023fca5dee2265
mais <95695358+maisl0l@users.noreply.github.com>
2023-01-23 02:39:19 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     #Install pacman packages
-    sudo pacman -Sy --noconfirm qemu fish flameshot thunderbird feh qrencode doge man iwd figlet cowsay cmatrix asciiquarium mpd bat cargo ranger ffmpeg mpv discord xclip libqalculate iftop htop aspell scrot dunst android-file-transfer brightnessctl slock network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp zsh spotifyd libreoffice
+    sudo pacman -Sy --noconfirm grep curl fzf sed awk qemu fish flameshot thunderbird feh qrencode doge man iwd figlet cowsay cmatrix asciiquarium mpd bat cargo ranger ffmpeg mpv discord xclip libqalculate iftop htop aspell scrot dunst android-file-transfer brightnessctl slock network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp zsh spotifyd libreoffice
     
     # xdg-user-dirs pipewire pipewire-alsa pipewire-jack pipewire-pulse qjackctl toilet ohmyzsh hollywood
     
