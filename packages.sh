a2639d04a1e6cb90b457297e6858589f17b10276
maisl0l <marclol@protonmail.com>
2023-03-22 23:29:44 +0100

    updated package list

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     #Install pacman packages
-    sudo pacman -Sy --noconfirm lmms obs-studio steam ttf-dejavu nerd-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts thefuck  python-pip grep curl fzf sed awk qemu fish flameshot thunderbird feh qrencode doge man iwd figlet cowsay cmatrix asciiquarium mpd bat cargo ranger ffmpeg mpv discord xclip libqalculate iftop htop aspell scrot dunst android-file-transfer brightnessctl slock network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp zsh spotifyd libreoffice
+    sudo pacman -Sy --noconfirm wine wine-gecko wine-mono winetricks wireshark-qt lutris aircrack-ng lmms obs-studio steam ttf-dejavu nerd-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts thefuck  python-pip grep curl fzf sed awk qemu fish flameshot thunderbird feh qrencode doge man iwd figlet cowsay godot cmatrix asciiquarium mpd bat cargo ranger ffmpeg mpv discord xclip libqalculate iftop htop aspell scrot dunst android-file-transfer brightnessctl slock network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp zsh spotifyd libreoffice
     
     # xdg-user-dirs ohmyzsh hollywood
     
@@ -14,8 +14,8 @@
     yay --version
     
     #Install yay packages
-    yay -S cava ani-cli-git ly spotify-tui doomretro gotop ngrok spacefm bullshit spotify vscodium remotemouse toilet discord-screenaudio betterdiscord-installer 
+    yay -S cava ani-cli-git ly spotify-tui doomretro gotop ngrok spacefm bullshit spotify heroic-games-launcher-bin vscodium remotemouse toilet discord-screenaudio betterdiscord-installer 
     
-    #git: jerry
+    #git: jerry proton-ge wine-ge
     
     #pip: mov-cli shell-gpt 
