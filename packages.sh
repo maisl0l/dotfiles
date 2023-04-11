5fc57003519d7cd663342fdbd1837014eddd6e0d
mais <95695358+maisl0l@users.noreply.github.com>
2023-04-12 00:53:15 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
     #Install pacman packages
-    sudo pacman -Sy --noconfirm wine wine-gecko wine-mono winetricks wireshark-qt lutris aircrack-ng lmms obs-studio steam ttf-dejavu nerd-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts thefuck  python-pip grep curl fzf sed awk qemu fish flameshot thunderbird feh qrencode doge man iwd figlet cowsay godot cmatrix asciiquarium mpd bat cargo ranger ffmpeg mpv discord xclip libqalculate iftop htop aspell scrot dunst android-file-transfer brightnessctl slock network-manager-applet cbatticon volumeicon hacksaw shotgun tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp zsh spotifyd libreoffice
+    sudo pacman -Sy --noconfirm wireshark-qt aircrack-ng obs-studio steam ttf-dejavu nerd-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts thefuck python-pip grep curl fzf sed awk qemu flameshot thunderbird feh qrencode doge man iwd figlet cowsay godot cmatrix asciiquarium bat cargo ffmpeg mpv discord xclip libqalculate iftop htop aspell dunst android-file-transfer brightnessctl slock network-manager-applet cbatticon volumeicon tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp zsh spotifyd libreoffice
     
-    # xdg-user-dirs ohmyzsh hollywood
+    # xdg-user-dirs ohmyzsh hollywood wine wine-gecko wine-mono winetricks lutris ranger
     
     #Install yay
     mkdir /tmp/yay
