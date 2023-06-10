3322a39a488954ba3a69dabc7567b0c9ed38d7a2
mais <95695358+maisl0l@users.noreply.github.com>
2023-06-10 23:45:08 +0200

    Update packages.sh
    replace file manager

--- packages.sh
+++ packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
     #Install pacman packages
-    sudo pacman -Sy --noconfirm wireshark-qt aircrack-ng obs-studio steam ttf-dejavu nerd-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts thefuck python-pip grep curl fzf sed awk qemu flameshot thunderbird feh qrencode doge man iwd figlet cowsay godot cmatrix asciiquarium bat cargo ffmpeg mpv discord xclip libqalculate iftop htop aspell dunst android-file-transfer brightnessctl slock network-manager-applet cbatticon volumeicon tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp zsh spotifyd libreoffice
+    sudo pacman -Sy --noconfirm ranger wireshark-qt xdg-user-dirs thunar aircrack-ng obs-studio steam ttf-dejavu nerd-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts thefuck python-pip grep curl fzf sed awk qemu flameshot thunderbird feh qrencode doge man iwd figlet cowsay godot cmatrix asciiquarium bat cargo ffmpeg mpv discord xclip libqalculate iftop htop aspell dunst android-file-transfer brightnessctl slock network-manager-applet cbatticon volumeicon tesseract firefox kitty picom polybar i3-gaps neofetch vim lolcat nitrogen openssh openvpn rofi gimp zsh spotifyd libreoffice
     
-    # xdg-user-dirs ohmyzsh hollywood wine wine-gecko wine-mono winetricks lutris ranger
+    #  ohmyzsh hollywood wine wine-gecko wine-mono winetricks lutris 
     
     #Install yay
     mkdir /tmp/yay
@@ -14,7 +14,7 @@
     yay --version
     
     #Install yay packages
-    yay -S cava ani-cli-git ly spotify-tui doomretro gotop ngrok spacefm bullshit spotify heroic-games-launcher-bin vscodium remotemouse toilet discord-screenaudio betterdiscord-installer 
+    yay -S cava ani-cli-git ly spotify-tui doomretro gotop ngrok bullshit spotify heroic-games-launcher-bin vscodium remotemouse toilet discord-screenaudio betterdiscord-installer 
     
     #git: jerry proton-ge wine-ge
     
